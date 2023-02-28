1cf7ead8b9889f4e5dd24497d9dde827a84d193d
Maverik37 <julien.bensliman@gmail.com>
2023-02-28 09:19:45 +0100

    ajout du script d'import pour les couleurs

---
+++ CombinaisonCalculator/calculator/batch/import_grades.py
@@ -0,0 +1,100 @@
+#!/bin/env  python3.6
+
+import csv
+import os
+import sys
+import django
+
+#Configuration pour pouvoir importer les modèles django
+sys.path.append('/home/fourbasse/scripts/DJANGO/CombinaisonCalculator/CombinaisonCalculator/CombinaisonCalculator/')
+os.environ["DJANGO_SETTINGS_MODULE"] = "CombinaisonCalculator.settings"
+django.setup()
+
+from calculator.models import *
+
+# Initialisation de certaines variables
+
+file="/home/fourbasse/Téléchargements/grade.csv"
+
+# Définition des fonctions
+
+def is_already_present(color):
+    qs_color = Couleur.objects.filter(co_name=color)
+
+    if qs_color.exists():
+        return True
+    else:
+        return False
+
+def get_palier_carac(nb,carac):
+    try:
+        qs_palier=Palier.objects.filter(p_nombre_medailles=nb)
+        qs_caracteristique = Caracteristique.objects.filter(c_name=carac)
+
+        if qs_palier.exists() and qs_caracteristique.exists():
+            return qs_palier, qs_caracteristique
+    except Exception as e:
+        print(e)
+
+def add_color(nb,carac,color,value):
+    try:
+        print(color)
+        obj_color = Couleur()
+
+        #Instance Couleur
+
+        obj_color.co_name = color
+        
+        # Récupération de l'instance de palier
+        obj_palier = Palier.objects.get(p_nombre_medailles=nb)
+
+        #Récupération de l'instance de la couleur
+        obj_carac = Caracteristique.objects.get(c_name=carac)
+        print(obj_palier,obj_carac)
+        obj_color.co_palier = obj_palier
+        obj_color.co_caracteristique = obj_carac
+        obj_color.co_effet = value
+        obj_color.save()
+    except Exception as e:
+        print(e)
+
+#Corps principal du script
+with open(file,newline='') as csvfile:
+    csvreader = csv.reader(csvfile,delimiter=';')
+
+    for row in csvreader:
+        color = row[0]
+        is_color_present = is_already_present(color)
+        if not is_color_present:
+            print("im here")
+            #1er palier
+            nb_medaille_first_palier = row[1].split(' ')[0]
+            value_first_palier = row[1].split(' ')[2].replace('+','').replace('%','')
+            effet_affected_first_palier = row[1].split(' ')[3].replace('_',' ')
+            #2eme palier
+            nb_medaille_second_palier = row[2].split(' ')[0]
+            value_second_palier = row[2].split(' ')[2].replace('+','').replace('%','')
+            effet_affected_second_palier = row[2].split(' ')[3].replace('_',' ')
+            #3eme palier
+            nb_medaille_third_palier = row[2].split(' ')[0]
+            value_third_palier = row[2].split(' ')[2].replace('+','').replace('%','')
+            effet_affected_third_palier = row[2].split(' ')[3].replace('_',' ')
+
+            # ajout en base de la nouvelle couleur
+            print(color)
+            obj_color_first = Couleur()
+
+            #Instance Couleur 1er palier
+
+            obj_color_first.co_name = color
+            
+            # Récupération de l'instance de palier
+            obj_palier_first = Palier.objects.get(p_nombre_medailles=nb_medaille_first_palier)
+
+            #Récupération de l'instance de la couleur
+            obj_carac_first = Caracteristique.objects.get(c_name=effet_affected_first_palier)
+            obj_color_first.co_palier = obj_palier_first
+            obj_color_first.co_caracteristique = obj_carac_first
+            obj_color_first.co_effet = value_first_palier
+            obj_color_first.save()
+          
